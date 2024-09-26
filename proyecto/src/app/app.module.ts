21916340384d6647fb6ea68fabb2308bc3fb3e99
MonseCastanon <monsecastanon023@gmail.com>
2024-09-26 10:01:40 -0600

    MMM

--- proyecto/src/app/app.module.ts
+++ proyecto/src/app/app.module.ts
@@ -8,12 +8,10 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, NgModel } from '@angular/forms';
 import { FormularioComponent } from './auth/pages/formulario/formulario.component';
-import { MapComponent } from './auth/components/card/map/map.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MapComponent,
     //FormularioComponent,
   ],
   imports: [
